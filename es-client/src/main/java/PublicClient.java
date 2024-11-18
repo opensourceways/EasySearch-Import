8e6e302c556b659cc6cd1a3eba2edb9d1f069aef
leefan <980488905@qq.com>
2024-11-18 15:42:00 +0800

    euler数据为空的数据类型不删除

--- es-client/src/main/java/PublicClient.java
+++ es-client/src/main/java/PublicClient.java
@@ -13,6 +13,8 @@ import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
 import org.elasticsearch.search.Scroll;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -31,7 +33,22 @@ import java.util.Set;
 public class PublicClient {
 
     public static RestHighLevelClient restHighLevelClient;
+    public static void deleteByType(String index, String type) {
+        DeleteByQueryRequest request = new DeleteByQueryRequest(index);
+        request.setQuery(QueryBuilders.termQuery("type", type)); // 根据type删除类型文档
+        try {
+            // 执行删除请求并获取响应
+            BulkByScrollResponse response = restHighLevelClient.deleteByQuery(request, RequestOptions.DEFAULT);
 
+            // 处理响应
+            long deletedDocs = response.getDeleted();
+
+            System.out.println("index:" + index + ",type:" + type + ",Deleted documents: " + deletedDocs);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
     public static void CreateClientFormConfig(String configPath) throws Exception {
         Yaml yaml = new Yaml(new Constructor(YamlConfig.class));
         InputStream inputStream = new FileInputStream(configPath);
